2da5024a394d1e2ae610999a16139ed5b3d5985f
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-12-15 16:04:09 +0000

    added data to db and calling AI apis

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 // response codes adn errors:
 // https://developer.twitter.com/en/support/twitter-api/error-troubleshooting
+// API REFERENCE INDEX 
+// https://developer.twitter.com/en/docs/api-reference-index
 
 //import request to trigger the tweet at certain intervals (scheduled function call)
 //cron job
@@ -16,6 +18,7 @@ require('dotenv').config()
 
 // reference to document in firestore db
 const dbRef = admin.firestore().doc(process.env.DB_REFERENCE);
+const dbRef2 = admin.firestore().collection(process.env.DB_REFERENCE2);
 const callbackURL = process.env.CALLBACK_URL
 
 //init twitter api (using OAuth 2.0)
@@ -74,8 +77,12 @@ exports.callback = functions.https.onRequest(async (request,response) => {
 
 });
 
-// STEP 3 - Refresh tokens and post tweets
-// endpoint to do something with api data
+// STEP 3 - Refresh tokens, query tweets and add to db if not exisit using tweet id
+
+// add invididual tweet to db just like how its added to list of tweets
+// change structure to only store tweet id and data 
+// before adding check if tweet id exists 
+// do i need list of tweets or maybe i do and i can change it to a hashmap/hashset 
 exports.tweet = functions.https.onRequest(async (request,response)=>{
     const{refreshToken} = (await dbRef.get()).data();
 
@@ -90,17 +97,41 @@ exports.tweet = functions.https.onRequest(async (request,response)=>{
     //API SEARCH TWEET https://twittercommunity.com/t/from-a-query-resending-to-obtain-a-oembed-twitter-api-v2-electron-nodejs/164231
     // API SEARCH TWEET https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
     // BUILD QUERY - https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query#examples
-    var res =  await refreshedClient.v2.search("in_reply_to_tweet_id:1603018284282138634 to:m1guelpf")
+    // var res =  await refreshedClient.v2.search("in_reply_to_tweet_id:1603018284282138634 to:m1guelpf")
+    var res = await refreshedClient.v2.get('tweets/search/recent', 
+        { 
+            query: "in_reply_to_tweet_id:1603018284282138634 to:m1guelpf", 
+            max_results: 10, 
+        });
 
-    list_of_tweets = []
+
+    list_of_tweets = [] // might turn this into a dictionary
     if (res.meta.next_token == undefined){
-        response.send(res.data);
+        functions.logger.info("NExt token is undefiend");
+        res.data.map(item => {
+            list_of_tweets.push(item)
+        })
     }else{
+        functions.logger.info("NExt token is not undefiend");
         // using the next token for polling 
         //https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/paginate
         while (res.meta.next_token !== undefined) {
             //append data in res to list of tweets
-            list_of_tweets.push(res.data)
+            res.data.map(item => {
+
+                const data = {
+                    id: item.id,
+                    text: item.text,
+                    replied: false,
+                  };
+                
+                list_of_tweets.push(data)
+                // dbRef2.add({data});
+                // if (item.id not in firestore documents){
+                //     dbRef2.add({daya});
+                // }
+                
+            })
             res = await refreshedClient.v2.get('tweets/search/recent', 
                 { 
                     query: "in_reply_to_tweet_id:1603018284282138634 to:m1guelpf", 
@@ -109,9 +140,31 @@ exports.tweet = functions.https.onRequest(async (request,response)=>{
                 });
 
         }
-        response.send(list_of_tweets);
     }
 
-    response.send("Completed")
+    response.send(list_of_tweets);
+});
+
+
+exports.callAPI = functions.https.onRequest(async (request,response)=>{
+    const dbSnapshot = await dbRef2.get();
+    tweets = []
+
+
+    dbSnapshot.docs.map(doc => 
+        {
+        //call api with doc.data().text
+        tweets.push(doc.data())
+        })
+
+
+
+
+    if (dbSnapshot !== undefined){
+        response.send(tweets);
+    } else{
+        response.send([]);
+    }
+    
 
 });
